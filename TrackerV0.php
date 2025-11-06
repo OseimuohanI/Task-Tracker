2b8b99bb9719515c1672687ec4be3a9a339d0a46
OseimuohanI <oseimuohanitua@outlook.com>
2025-11-06 16:16:43 +0100

    Fully Functioning Task Tracker -json implementation needed

--- TrackerV0.php
+++ TrackerV0.php
@@ -3,6 +3,12 @@
     $increment = 0;
     $tasks = [];
     $progress = [];
+
+    function Authenticate($x){
+        if (isset($x) && !empty($x) || $x != ""){
+            return 1;
+        } else {return 0;}
+    }
        
     function AddTask(){
         global $tasks;
@@ -37,9 +43,9 @@
         if (count($tasks) == count($progress)){
             foreach ($progress as $key => $value){
                 if ($progress[$key] == 0){
-                    return " In progress";
+                    return "In progress";
                 } else if ($progress[$key] == 1){
-                    return " Task Completed";
+                    return "Task Completed";
                 } else {return "Error";}
             }
         }
@@ -49,31 +55,42 @@
         global $tasks;
         global $progress;
         if (count($tasks) == count($progress)) {
-            foreach ($tasks as $task) {return "Task " . $task . " " . determineProgress();}
+            foreach ($tasks as $task) {$i = 1; return $i++ . ". Task " . $task . ": " . determineProgress() . " ";}
         }
     }
 
-    if (isset($tasks)){
-        if ($tasks == []){
-            echo AddTask();
-        }else if (count($tasks) >= 1){
-            $input1 = readline("Would you like to review your tasks? y/n ");
-            if (isset($input1)){
-                if ($input1 == "y" || $input1 == "n" || $input1 == "N" || $input1 == "Y"){
-                    if ($input1 == "y" || $input1 == "Y"){
-                        echo printArray($tasks);
-                        $choice = readline("Which Task would you like to change status for? ");
-                            if (isset($choice)){
-                                $choice -= 1;
-                            }
-                    } else if ($input1 == "n" || $input1 == "N"){
-                        echo "Thank you for your Time";
+    for ($i = 1; $i <= 2; $i++){
+        if (isset($tasks)){
+            if ($tasks == []){
+                echo AddTask();
+            }else if (count($tasks) >= 1){
+                $input1 = readline(" Would you like to review your tasks? y/n ");
+                if (isset($input1)){
+                    if ($input1 == "y" || $input1 == "n" || $input1 == "N" || $input1 == "Y"){
+                        if ($input1 == "y" || $input1 == "Y"){
+                            echo printArray($tasks);
+                            $choice = readline(" Which Task would you like to change status for? ");
+                                if (isset($choice) && ctype_digit($choice) ){
+                                    $choice -= 1;
+                                    $choice = (int)$choice;
+                                    // echo $choice;
+                                    if ($progress[$choice] == 0){
+                                        $progress[$choice] = 1; echo printArray();
+                                    } else if ($progress[$choice] == 1){
+                                        $progress[$choice] = 0; echo printArray();
+                                    } else{ echo "Error";}
+                                } else {
+                                    echo "Input a number";
+                                }
+                        } else if ($input1 == "n" || $input1 == "N"){
+                            echo "Thank you for your Time";
+                        }
+                    } else{
+                        echo "Retry";
                     }
-                } else{
+                } else {
                     echo "Retry";
                 }
-            } else {
-                echo "Retry";
             }
         }
     }
