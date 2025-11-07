c4c629ff10c3031f683cb0064a27d91fe478e724
OseimuohanI <oseimuohanitua@outlook.com>
2025-11-07 06:33:27 +0100

    Began dev of V1

---
+++ TrackerV1.php
@@ -0,0 +1,14 @@
+<?php
+    class Tasks{
+        public $id;
+        public $des;
+        public $stat;
+        public $createdAt;
+        public $updatedAt;
+
+        public function __construct($id)
+        {
+            
+        }
+    }
+?>
