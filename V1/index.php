742b7ba0da81c9f603c31c1cfdc42073667e3cc7
OseimuohanI <oseimuohanitua@outlook.com>
2025-11-10 06:41:06 +0100

    index needed to direct

--- V1/index.php
+++ V1/index.php
@@ -5,6 +5,10 @@
         sleep(1.5);
         header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
         exit();
-    } else {sleep(1.5); header('location: input.php'); exit();}
+    }
+    
+    sleep(1);
+    header('location: input.php');
+    exit();
     
 ?>
